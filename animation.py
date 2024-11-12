7927a4f80cb7b4b987d3244fcb9d35dc5ae843a0
Ctrl-Jesse-David <jdfrancisco5@up.edu.ph>
2024-11-12 15:57:07 +0800

    Add files via upload

---
+++ animation.py
@@ -0,0 +1,50 @@
+from utilities import clear_screen
+import time
+
+# Function that display's the egg roll (indiviudally)
+def egg_rolling_animation():
+    egg = "🥚"
+    frames = 53  # Number of frames to roll across the screen
+
+    for position in range(frames):
+        clear_screen()
+        print("=======================================================")
+        print("                🍳 Egg Roll Challenge 🍳")
+        print("=======================================================\n")
+        
+        # Move the egg across the screen in each frame
+        print(" " * position + egg)
+        print(" " * (frames-position) + egg)
+        print(" " * position + egg)
+        print(" " * (frames-position) + egg)
+        print(" " * position + egg)
+        print(" " * (frames-position) + egg)
+
+        print("\n-------------------------------------------------------")
+        print("Loading...\n")
+        time.sleep(0.01)  # Control the animation speed
+
+# Function that displays the loading animation
+def loading_animation():
+    egg = "🥚"
+    frames = 50  # Number of frames to roll across the screen
+
+    for position in range(frames):
+        clear_screen()
+        print("=======================================================")
+        print("              🍳 Egg Roll Challenge 🍳")
+        print("=======================================================\n")
+        
+        # Move the egg across the screen in each frame
+        print(" " * position + egg)
+        print(" " * position + egg)
+        print(" " * position + egg)
+        
+        print("\nLoading Menu...")
+        print("-------------------------------------------------------")
+        time.sleep(0.01)  # Control the animation speed
+
+# Function for game's introduction
+def egg_rolling_animation_intro():
+    egg_rolling_animation()
+    egg_rolling_animation()
