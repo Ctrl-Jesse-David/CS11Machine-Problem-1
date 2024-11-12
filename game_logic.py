2612888f09eb09565cbffd8e593ed7d53a16e94d
Ctrl-Jesse-David <jdfrancisco5@up.edu.ph>
2024-11-12 15:17:47 +0800

    Update game_logic.py

--- game_logic.py
+++ game_logic.py
@@ -5,21 +5,32 @@ from egg_mover import move_eggs
 import time
 
 
+wall = '🧱'
+egg = '🥚'
+grass = '🟩'
+empty_nest = '🪹'
+full_nest = '🪺'
+frying_pan = '🍳'
+
+
 def copy_grid(grid): # Copies the grid for the undo
     return [row[:] for row in grid]
-
 
 # Main game loop
 def start_game(filename):
     from main import main_menu
     grid, num_moves = load_level(filename) # Unpack/Read level's file
     score = 0 # Score intializer
-    all_eggs_in_nests = False # Egg's location tracker
     history = [] # Moves history for [U]ndo
     moves = [] # For moves history
+    movement_history = {
+        'L': "←",
+        'R': "→",
+        'F': "↑",
+        'B': "↓"
+    }
 
-    while num_moves > 0 and not all_eggs_in_nests:
-        # Length of header and footer should be dependent on the size of the column of the gird
+    while num_moves > 0:
         clear_screen()
         print("=======================================================")
         cprint("              🍳 EGG ROLL CHALLENGE 🍳", "blue", attrs = ["bold"])
@@ -28,7 +39,7 @@ def start_game(filename):
         print("-------------------------------------------------------")
         print(colored("🔢 MOVES LEFT  ", "blue") + f" :  {num_moves}")
         print(colored("🌟 SCORE       ", "blue") + f" :  {score}")
-        print(colored("🎯 PREV MOVE   ", "blue") + f" :  {moves}")
+        print(colored("🎯 PREV MOVE   ", "blue") + f" :  {" ".join(moves).strip()}")
         print("-------------------------------------------------------")
         move = input("Enter move " + colored("(L/R/F/B)", "blue", attrs = ["bold"]) 
                      + ", " + colored("[U]", "blue", attrs = ["bold"]) + "ndo, or " 
@@ -36,7 +47,8 @@ def start_game(filename):
         print("-------------------------------------------------------")
 
         if move == 'U' and history:
-            grid, num_moves, score = history.pop()
+            grid, num_moves, score, moves = history.pop()
+            moves.pop()
             continue
         elif move == 'E':  # Handle exit here
             while True:
@@ -55,29 +67,39 @@ def start_game(filename):
             time.sleep(1)
             continue
 
-        history.append((copy_grid(grid), num_moves, score))
-        add_score, all_eggs_in_nests = move_eggs(grid, move)
+        moves.append(movement_history[move])
+        history.append((copy_grid(grid), num_moves, score, moves))
+        add_score = move_eggs(grid, move)
         score += add_score
         num_moves -= 1
     
-    clear_screen()
-    if all_eggs_in_nests:
-        score += num_moves # Add remaining moves after the game
-        print("=======================================================")
-        cprint("            🎉🎉 CONGRATULATIONS! 🎉🎉", "green", attrs = ["bold"])
-        print("=======================================================")
-        cprint("   🏆🏅 All eggs safely rolled into their nests! 🏅🏆", attrs = ["bold"])
-        print("-------------------------------------------------------")
-        print(colored("🏆 FINAL SCORE: ", "green", attrs = ["bold"]) + f" {score} 🏆")
-        print("-------------------------------------------------------\n")
-    else:
-        print("=======================================================")
-        cprint("                  💥😞 You lost! 💥😞 ", "red", attrs = ["bold"])
-        print("=======================================================")
-        cprint("              better luck next time!", attrs = ["bold"])
-        print("-------------------------------------------------------")
-        print(colored("🏆 FINAL SCORE: ", "red", attrs = ["bold"]) + f" {score} 🏆")
-        print("-------------------------------------------------------\n")
+        clear_screen()
+    
+        # Check whether you won or lost
+        egg_positions = [(row, col) for row in range(len(grid)) for col in range(len(grid[0])) if grid[row][col] == egg]
+        full_nest_positions = [(row, col) for row in range(len(grid)) for col in range(len(grid[0])) if grid[row][col] == full_nest]
+        
+        if not egg_positions:  # No more eggs in the frid
+            if full_nest_positions: # If there is at least one full nest
+                clear_screen()
+                print("=======================================================")
+                cprint("            🎉🎉 CONGRATULATIONS! 🎉🎉", "green", attrs = ["bold"])
+                print("=======================================================")
+                cprint("   🏆🏅 All eggs safely rolled into their nests! 🏅🏆", attrs = ["bold"])
+                print("-------------------------------------------------------")
+                print(colored("🏆 FINAL SCORE: ", "green", attrs = ["bold"]) + f" {score} 🏆")
+                print("-------------------------------------------------------\n")
+                break
+            else: # No full nest and every egg went to the frying pan
+                clear_screen()
+                print("=======================================================")
+                cprint("                  💥😞 You lost! 💥😞 ", "red", attrs = ["bold"])
+                print("=======================================================")
+                cprint("              better luck next time!", attrs = ["bold"])
+                print("-------------------------------------------------------")
+                print(colored("🏆 FINAL SCORE: ", "red", attrs = ["bold"]) + f" {score} 🏆")
+                print("-------------------------------------------------------\n")
+                break
 
     display_grid(grid)
     print("-------------------------------------------------------")
