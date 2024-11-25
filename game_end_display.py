fcd286debac082bafc120cbbd7d09d5551d05a23
Ctrl-Jesse-David <jdfrancisco5@up.edu.ph>
2024-11-25 19:03:02 +0800

    Update game_end_display.py

--- game_end_display.py
+++ game_end_display.py
@@ -1,42 +1,60 @@
 from termcolor import cprint, colored
 from utilities import display_grid
 
-# The displays that will pop-up after finishing the game:
+def display_header(title, title_color="green"):
+    """ Displays the header with a changeable title """
+    print("=======================================================")
+    cprint(f"            {title}", title_color, attrs=["bold"])
+    print("=======================================================")
 
-def display_victory(score): # If you were able to put all eggs inside the nest
-    print("=======================================================")
-    cprint("            🎉🎉 CONGRATULATIONS! 🎉🎉", "green", attrs=["bold"])
-    print("=======================================================")
-    cprint("   🏆🏅 All eggs safely rolled into their nests! 🏅🏆", attrs=["bold"])
+def display_footer(score, color="green"):
+    """ Displays the footer with the final score of the player """
     print("-------------------------------------------------------")
-    print(colored("🏆 FINAL SCORE: ", "green", attrs=["bold"]) + f" {score} 🏆")
+    print(colored("🏆 FINAL SCORE: ", color, attrs=["bold"]) + f"{score} 🏆")
     print("-------------------------------------------------------\n")
 
-def display_partial_victory(score): # If you weren't able to put ALL eggs inside the nests (but was able to put at least 1)
-    print("=======================================================")
-    cprint("                  💥😞 Nice Try! 💥😞", "red", attrs=["bold"])
-    print("=======================================================")
-    cprint("  You weren't able to put all eggs inside the nest!!", attrs=["bold"])
-    print("-------------------------------------------------------")
-    print(colored("🏆 FINAL SCORE: ", "red", attrs=["bold"]) + f" {score} 🏆")
-    print("-------------------------------------------------------\n")
+def display_message(score, title, main_message, title_color="green", score_color="green"):
+    """ Displays the victory/partial victory/loss/out of moves message """
+    display_header(title, title_color)
+    cprint(main_message, attrs=["bold"])
+    display_footer(score, score_color)
 
-def display_loss(score): # If you weren't able to put EVERY egg inside the nests
-    print("=======================================================")
-    cprint("                  💥😞 You lost! 💥😞", "red", attrs=["bold"])
-    print("=======================================================")
-    cprint("                 Better luck next time!", attrs=["bold"])
-    print("-------------------------------------------------------")
-    print(colored("🏆 FINAL SCORE: ", "red", attrs=["bold"]) + f" {score} 🏆")
-    print("-------------------------------------------------------\n")
+def display_victory(score):
+    """ Displays the victory message """
+    display_message(
+        score,
+        title="🎉🎉 CONGRATULATIONS! 🎉🎉",
+        main_message="   🏆🏅 All eggs safely rolled into their nests! 🏅🏆",
+        title_color="green",
+        score_color="green"
+    )
 
-def display_out_of_moves(score, grid): # If you ran out of moves
-    print("=======================================================")
-    cprint("             💥😞 You ran out of moves! 💥😞", "red", attrs=["bold"])
-    print("=======================================================")
-    cprint("                Better luck next time!", attrs=["bold"])
-    print("-------------------------------------------------------")
-    print(colored("🏆 FINAL SCORE: ", "red", attrs=["bold"]) + f" {score} 🏆")
-    print("-------------------------------------------------------\n")
-    display_grid(grid)
-    print("-------------------------------------------------------")
+def display_partial_victory(score):
+    """ Displays the partial victory message """
+    display_message(
+        score,
+        title="💥😞 Nice Try! 💥😞",
+        main_message="  You weren't able to put all eggs inside the nest!!",
+        title_color="red",
+        score_color="red"
+    )
+
+def display_loss(score):
+    """ Displays the loss message """
+    display_message(
+        score,
+        title="💥😞 You lost! 💥😞",
+        main_message="                 Better luck next time!",
+        title_color="red",
+        score_color="red"
+    )
+
+def display_out_of_moves(score, grid):
+    """ Displays the out of moves message """
+    display_message(
+        score,
+        title="💥😞 You ran out of moves! 💥😞",
+        main_message="                Better luck next time!",
+        title_color="red",
+        score_color="red"
+    )
